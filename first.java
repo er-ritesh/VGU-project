a585050e7f3fa0b2d583c2c03c217f4955ce1a30
Ritesh Sharma <textriteshsharma@gmail.com>
2024-09-20 13:10:03 +0530

    new files

---
+++ first.java
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ritesh</title>
+    <style>
+        button{
+            text-align: center; align-items: center; 
+        }
+    </style>
+</head>
+<body>
+    <p class="second" style="background-color: coral;">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, placeat quam illo, cumque commodi nisi distinctio molestias modi numquam maxime dolorem laudantium. Voluptates nesciunt, accusamus repudiandae asperiores animi eligendi adipisci.
+    </p>
+    <H1 style="background-color: olive; padding: 5%; width: fit-content;">Ritesh</H1>
+    <br>
+    <button>Start</button>
+    <h2>vgu</h2>
+    <hr>
+    <h3>Jaipur</h3>
+    <a href="https://riteshsharmaa.blogspot.com/">blog</a>
+    <img src="op1.png" alt="op1">
+    <H1>Rajasthan</H1>
+    <pre>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit quisquam cum aliquam autem impedit saepe! Enim praesentium, porro reiciendis dolores ducimus animi tempora sunt suscipit, aspernatur possimus explicabo dicta impedit!</pre>
+    <input type="text here" title="ritesh">
+
+<div>
+    <select name="Student's Name" id="select">
+        <option value="Select the Student">Select the Student</option>
+        <option value="Sujay">Sujay</option>
+        <option value="Manvee">Manveer</option>
+
+    </select>
+    <legend>GENDER:
+        Male <input type="radio" name="GENDER">
+        Female <input type="radio" name="GENDER">
+    </legend>
+</div>
+</body>
+</html>
